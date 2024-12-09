79fbd1a8d373e297d6ebdc2d87eb9d1677b20efa
Sukhdev Thakur <devil51sukhdev@gmail.com>
2024-12-09 13:30:06 +0545

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AddEmployeeForm from "./components/AddEmployeeForm";
+import UpdateEmployee from "./components/UpdateEmployee";
+import EmployeeList from "./components/EmployeeList";
+// import HeaderLayout from "./components/HeaderLayout.jsx";
+import Layout from "./components/HeaderLayout.jsx";
 
-const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/Home" element={<Home />} />
-    </Routes>
-  </Router>
-);
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout><Home /></Layout> } />
+        <Route path="/add-employee" element={<Layout><AddEmployeeForm /></Layout>} />
+        <Route path="/employee/update/:id" element={<Layout><UpdateEmployee /></Layout>} />
+        <Route path="/employee-list" element={<Layout><EmployeeList /></Layout>} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,35 @@
-import AddEmployeeForm from "../components/AddEmployeeForm"
-import EmployeeList from "../components/EmployeeList"
+import React from "react";
+import { Link } from "react-router-dom";
 
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center">
+      
+      
+      <div className="flex space-x-4">
+        {/* Add Employee Button */}
+        <Link to="/add-employee">
+          <button className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
+            Add Employee
+          </button>
+        </Link>
 
+        {/* Update Employee Button */}
+        {/* <Link to="/employee/update/:id">
+          <button className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
+            Update Employee
+          </button>
+        </Link> */}
 
-const Home = () =>{
-    return(<div>
-        <h1> Employee Management</h1>
-        <AddEmployeeForm/>
-        <EmployeeList/>
+        {/* Employee List Button */}
+        <Link to="/employee-list">
+          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+            Employee List
+          </button>
+        </Link>
+      </div>
     </div>
-    );
+  );
 };
 
 export default Home;
